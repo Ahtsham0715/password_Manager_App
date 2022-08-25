3afffd26f62cb536fbc1307ff9ad2ca201e61e26
Ahtsham0715 <ahtsham50743@gmail.com>
2022-08-25 20:16:35 +0500

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:password_manager/firebase_options.dart';
 import 'package:password_manager/loginscreen.dart';
 import 'dart:async';
-import 'package:flutter_gradients/flutter_gradients.dart';
+import 'package:password_manager/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,67 +18,21 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Password Manager',
+    return NeumorphicApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.blueGrey,
-        brightness: Brightness.light,
+      title: 'Password Manager',
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        // baseColor: Color(0xFFFFFFFF),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatefulWidget {
-  @override
-  _FirstPageState createState() => _FirstPageState();
-}
-
-class _FirstPageState extends State<FirstPage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        Duration(seconds: 2),
-        (){}
-            );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: ShapeDecoration(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(0.0),
-          ),
-          gradient: FlutterGradients.warmFlame(
-            tileMode: TileMode.decal,
-            type: GradientType.sweep,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.lock,
-                size: 250.0,
-              ),
-              Text(
-                "Password Manager",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35.0,
-                ),
-              ),
-            ],
-          ),
-        ),
+      darkTheme: NeumorphicThemeData(
+        // baseColor: Color(0xFF3E3E3E),
+        lightSource: LightSource.topLeft,
+        depth: 6,
       ),
+      home: SplashScreen(),
     );
   }
 }
